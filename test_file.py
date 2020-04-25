0690c0ea4e00a6369c676b60eaaae51b8f1076cc
aliabd <ali.si3luwa@gmail.com>
2020-04-25 04:36:50 -0700

    sample changes to functions code and docs

--- test_file.py
+++ test_file.py
@@ -5,16 +5,16 @@ def sum_function(a, b):
     :param b: integer
     :return: sum of a and b
     """
-    c = a + b
+    c = a * b
     return c
 
 
 def average_function(a, b):
     """
-    returns the average of a and b
+    returns the average of the squares of a and b
     :param a: integer
     :param b: integer
     :return: average of a and b
     """
-    c = (a + b) / 2
+    c = (a**2 + b**2) / 2
     return c
