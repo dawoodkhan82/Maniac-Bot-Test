ca4c77a636481c688eb99730b434eb9834172ce3
Dawood Khan <dawoodkhan82@gmail.com>
2020-04-28 23:17:23 -0700

    Update test_file.py

--- test/test_file.py
+++ test/test_file.py
@@ -10,8 +10,8 @@ def sum_function(a, b, c):
     :param b: integer
     :return: sum of a and b
     """
-    ca = a * b * a 
-    return ca
+    c = a * b * a 
+    return c
 
 
 def average_function(a, b):
